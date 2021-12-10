f7d7c398a599666ba52435a33ea4f24c54e5a502
Sjoerd Stottelaar <stottelaar.s@gmail.com>
2021-12-10 12:15:52 +0100

    Update migration script and build target file

---
+++ TailKit/Migrations/PackageMigrationPlan.cs
@@ -0,0 +1,11 @@
+using Umbraco.Cms.Infrastructure.Packaging;
+
+namespace TailKit.Migrations
+{
+    public class PackageMigrationPlan : AutomaticPackageMigrationPlan
+    {
+        public PackageMigrationPlan() : base("TailKit")
+        {
+        }
+    }
+}
